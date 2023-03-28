0c6fc7dd2139fd264a46db552cea141de0d983cc
JeroenFredriksz <you@example.com>
2023-03-28 12:07:03 +0200

    entiteit gerecht aangemaakt zonder relaties, wordt nog niet toegevoegd aan de database

---
+++ src/Infrastructure/Persistence/Configurations/GerechtConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Template.Domain.Entities;
+
+namespace Template.Infrastructure.Persistence.Configurations;
+
+public class GerechtConfiguration : IEntityTypeConfiguration<Gerecht>
+{
+	public void Configure(EntityTypeBuilder<Gerecht> builder)
+	{
+		
+	}
+}
