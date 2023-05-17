4e4d5fa1400bec7a894e0e8cf206ae400b444a7e
JefrDev <Jeroen.fredriksz@gmail.com>
2023-05-17 22:55:14 +0200

    GetFavoritesById naar de goede controller gehaald en route verbeterd

--- src/Presentation/Controllers/RecipesController.cs
+++ src/Presentation/Controllers/RecipesController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Template.Application.Common.Models;
 using Template.Application.Dtos;
 using Template.Application.Logic.Recipes.Queries;
+using Template.Application.Logic.Users.Queries;
 
 namespace Template.Presentation.Controllers;
 
@@ -17,4 +18,9 @@ public class RecipesController : ApiControllerBase
 		{
 			RecipeId = id
 		});
+	
+	[HttpGet("favorite")]
+	public async Task<ActionResult<PaginatedList<RecipeDto>>> GetFavoritesById([FromQuery] GetFavoritesByIdQuery query, CancellationToken cancellationToken) => 
+		Ok(await Mediator.Send(query, cancellationToken));
 }
+
--- src/Presentation/Controllers/UsersController.cs
+++ src/Presentation/Controllers/UsersController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Template.Application.Common.Models;
-using Template.Application.Dtos;
 using Template.Application.Logic.Users.Commands;
-using Template.Application.Logic.Users.Queries;
 
 namespace Template.Presentation.Controllers;
 
@@ -16,8 +13,5 @@ public class UsersController : ApiControllerBase
 		await Mediator.Send(command, cancellationToken);
 		return NoContent();
 	}
-
-	[HttpGet("favoriterecipes")]
-	public async Task<ActionResult<PaginatedList<RecipeDto>>> GetFavoritesById([FromQuery] GetFavoritesByIdQuery query, CancellationToken cancellationToken) => 
-		Ok(await Mediator.Send(query, cancellationToken));
-	}
+}
+	
